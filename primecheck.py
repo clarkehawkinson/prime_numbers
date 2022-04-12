e891d85e7ddef188f882a8c1757827ad4f31f32b
clarkehawkinson <provojuggler@gmail.com>
2022-04-12 12:30:07 -0600

    Writing to CSV and optimizing search
    ###---You need to create your own prime.csv file before you run this code---###
    Added the sqrt function to limit search
    Changed range to (3,n,2) to search all odd numbers only
    Added a list to write prime numbers to.
    Added csv.writer to save all prime numbers for further analysis.

--- primecheck.py
+++ primecheck.py
@@ -1,11 +1,19 @@
 #This code is meant to find all the prime numbers between 1 and n.
-n = 50       ##Number to check up to
-mid = int((n+1)/2)    #Optimizing the division part of the search, Look into sqrt(n)+1
+import math
+import csv
 
-for y in range(2,n):     #looping through the numbers between 2 and n
+list = []   #array for the prime numbers
+n = 10000      ##Number to check up to
+mid = int(math.sqrt(n)+1)    #Optimizing the division part of the search, Look into sqrt(n)+1
+
+for y in range(3,n,2):     #looping through the numbers between 3 and n checking odd numbers only
     for i in range(2,mid):       #getting the numbers to divide the search by
         if y % i  == 0 and y!=i:    ##checking to see if the remainder of n/i is 0
             # print("Not prime", n, i, y) #Checking to see if it is prime or not and then printing, uncomment during debugging only
             break       #If it is divisable by anything, then go to next number
     else:
-       print(y)     #printing the prime numbers
+        list.append(y)
+print(list)
+with open('prime.csv','w',newline='') as csvfile:   #Writing the list to a csv file
+    primewriter = csv.writer(csvfile, delimiter = ',')
+    primewriter.writerow(list)
